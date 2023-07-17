ff6fc9e9cc6ed021b2084ba7fa5456e8879611ec
matsudamper <let@matsudamper.net>
2023-07-18 03:30:23 +0900

    個別機能に対してもコードを実行する

--- src/test/java/GenerateReflectionConfig.kt
+++ src/test/java/GenerateReflectionConfig.kt
@@ -1,15 +1,64 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
+import io.ktor.server.cio.CIO
+import io.ktor.server.engine.embeddedServer
 import net.matsudamper.portforward.main
+import net.matsudamper.portforward.server.myApplicationModule
 import org.junit.jupiter.api.Test
 
 class GenerateReflectionConfig {
     @Test
-    fun generate() = runBlocking {
-        main(
-            arrayOf(
-                "--port=8080",
-                "--config=config.yml",
-            ),
-        )
+    fun main() {
+        val scope = CoroutineScope(Job())
+        val job = scope.launch {
+            main(
+                arrayOf(
+                    "--port=8080",
+                    "--config=config.yml",
+                ),
+            )
+        }
+
+        runCatching {
+            runBlocking {
+                withTimeout(5000) {
+                    awaitCancellation()
+                }
+            }
+        }
+
+        job.cancel()
+    }
+
+    @Test
+    fun startAppServer(): Unit = runBlocking {
+        runCatching {
+            withTimeout(5000) {
+                val scope = this
+                embeddedServer(
+                    CIO,
+                    port = 9090,
+                    module = {
+                        myApplicationModule(
+                            onStart = {
+                                scope.cancel()
+                            },
+                            onStop = {},
+                        )
+                    },
+                    configure = {
+                    },
+                ).start(wait = true)
+            }
+        }
     }
 }
